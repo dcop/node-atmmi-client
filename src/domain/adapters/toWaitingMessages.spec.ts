c0f10398f5c0b4c286ea2e392ba0eebce5db3d88
dcop <daniele.coppolino@gmail.com>
2020-03-08 22:49:34 +0100

    More test for adapter

--- src/domain/adapters/toWaitingMessages.spec.ts
+++ src/domain/adapters/toWaitingMessages.spec.ts
@@ -3,7 +3,7 @@ import { GeoDataDTOBuilder } from "../../stubs/__builders/GeoDataDTOBuilder";
 import { GeoDataLineBuilder } from "../../stubs/__builders/GeoDataLineBuilder";
 
 describe("to waiting messages adapter", () => {
-  it("should adapt", () => {
+  it("with standard timing", () => {
     const geoData = GeoDataDTOBuilder
       .aGeoData()
       .withLine(GeoDataLineBuilder
@@ -17,6 +17,45 @@ describe("to waiting messages adapter", () => {
 
     expect(adapted).toStrictEqual([{
       alerts: [],
+      line: "56",
+      waitingTimeInMinutes: 10
+    }])
+  })
+
+  it("with incoming line", () => {
+    const geoData = GeoDataDTOBuilder
+      .aGeoData()
+      .withLine(GeoDataLineBuilder
+        .aGeoDataLine()
+        .withLineCode("56")
+        .withWaitMessage("In arrivo")
+        .build()
+      )
+      .build();
+    const adapted = toWaitingMessages(geoData)
+
+    expect(adapted).toStrictEqual([{
+      alerts: [],
+      line: "56",
+      waitingTimeInMinutes: 0
+    }])
+  })
+
+  it("with alerts", () => {
+    const geoData = GeoDataDTOBuilder
+      .aGeoData()
+      .withLine(GeoDataLineBuilder
+        .aGeoDataLine()
+        .withLineCode("56")
+        .withWaitMessage("10 minuti")
+        .withTrafficBulletin("Bla bla")
+        .build()
+      )
+      .build();
+    const adapted = toWaitingMessages(geoData)
+
+    expect(adapted).toStrictEqual([{
+      alerts: ["Bla bla"],
       line: "56",
       waitingTimeInMinutes: 10
     }])
--- src/stubs/__builders/GeoDataLineBuilder.ts
+++ src/stubs/__builders/GeoDataLineBuilder.ts
@@ -1,11 +1,14 @@
 import { GeoDataLine } from "../../dto/GeoDataDTO";
 import geoData from '../geoData.json'
+import { TrafficBulletin } from "../../dto/StopInfoDTO";
 
 export class GeoDataLineBuilder {
   private waitMessage: string;
   private lineCode: string;
+  private bulletins: TrafficBulletin[];
 
   constructor() {
+    this.bulletins = []
   }
 
   static aGeoDataLine() {
@@ -24,6 +27,17 @@ export class GeoDataLineBuilder {
     return this;
   }
 
+  withTrafficBulletin(bulletin: string): this {
+    this.bulletins.push({
+      Body: bulletin,
+      ExpirationDate: null,
+      PublicationDate: null,
+      Title: "A_TITLE"
+    });
+
+    return this
+  }
+
   build(): GeoDataLine {
     return {
       ...geoData.Lines[0],
@@ -31,7 +45,8 @@ export class GeoDataLineBuilder {
       Line: {
         ...geoData.Lines[0].Line,
         LineCode: this.lineCode
-      }
+      },
+      TrafficBulletins: this.bulletins
     }
   }
 }
