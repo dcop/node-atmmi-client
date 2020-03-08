09758730137b86158704259faafa1d057ed4d017
dcop <daniele.coppolino@gmail.com>
2020-03-09 00:07:56 +0100

    Deps upgrade and moved index.ts outside src

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { AtmClient } from "./src/client/AtmClient";
+import { toWaitingMessages } from "./src/domain/adapters/toWaitingMessages";
+
+export {
+  AtmClient as default,
+  toWaitingMessages
+};
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { AtmClient } from "./client/AtmClient";
-import { toWaitingMessages } from "./domain/adapters/toWaitingMessages";
-
-export {
-  AtmClient as default,
-  toWaitingMessages
-};
